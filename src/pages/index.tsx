e70046521b1a41de5d4a47ffb87b3a453120a847
divergen371 <divergen@protonmail.com>
2021-07-18 22:01:23 +0900

    Implemented the login screen in index.tsx.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,3 +2,49 @@ import { Auth, Button, IconLogOut } from '@supabase/ui'
 import type { ReactNode } from 'react'
 import React from 'react'
 import { LayoutWrapper } from '../components/layoutWrapper'
+import { client } from '../lib/supabase'
+
+type Props = {
+  children: ReactNode
+}
+
+const Container = (props: Props) => {
+  const { user } = Auth.useUser()
+  if (user) {
+    return (
+      <div>
+        <div className="flex justify-end mx-2 my-4">
+          <Button
+            size="medium"
+            icon={<IconLogOut />}
+            onClick={() => client.auth.signOut()}
+          >
+            Sign out
+          </Button>
+        </div>
+      </div>
+    )
+  }
+  // ログインしていない場合
+  return <>{props.children}</>
+}
+
+const Home = () => {
+  return (
+    <LayoutWrapper>
+      <Auth.UserContextProvider supabaseClient={client}>
+        <Container>
+          <div className="flex justify-center pt-8">
+            <div className="w-full sm:w-96">
+              <Auth
+                supabaseClient={client}
+                providers={['github']}
+                socialColors={true}
+              />
+            </div>
+          </div>
+        </Container>
+      </Auth.UserContextProvider>
+    </LayoutWrapper>
+  )
+}
