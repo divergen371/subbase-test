ac7fbfc3a5a3dd36b72a85f0406e3cc77258d7c6
divergen371 <divergen@protonmail.com>
2021-07-18 21:48:35 +0900

    Introduced Eslint and Prettier.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  eslint: {
+    dirs: [
+      'src/',
+      'src/pages/',
+      'src/pages/api',
+      'src/components/',
+      'src/lib/',
+    ],
+  },
 }
